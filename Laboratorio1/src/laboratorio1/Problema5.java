7f07f88536388f74e26500eaca365f2c418079b8
Jeffer <Jeffer@DESKTOP-09GEI3K.home>
2016-08-31 21:24:49 -0500

    Listo el problema 5

--- Laboratorio1/src/laboratorio1/Problema5.java
+++ Laboratorio1/src/laboratorio1/Problema5.java
@@ -32,6 +32,7 @@ public class Problema5 {
                 char letrafila = letraFila(filapuestos);
                 System.out.println("Hay puestos en la fila " + letrafila + " de la silla " + puestoinicial
                         + " a la " + puestofinal);
+                cantpuestosdisponibles = 0;
             }
             centinela = false;
         }
@@ -58,15 +59,12 @@ public class Problema5 {
                 char letrafila = letraFila(filapuestos);
                 System.out.println("La reservación está en la fila " + letrafila + " de la silla " + puestoinicial
                         + " a la " + puestofinal);
+                puestosreservados = 0;
             } else {
 
             }
             centinela = false;
         }
-    }
-
-    static int[] addTARCINE(int[] tarcine) {
-        return addInfo(70000, tarcine);
     }
 
     static int[] addInfo(int info, int[] array) {
@@ -76,11 +74,6 @@ public class Problema5 {
         }
         arraysustituto[array.length] = info;
         return arraysustituto;
-    }
-
-    static int[] addID(int[] id, int numid) {
-        return addInfo(numid, id);
-
     }
 
     static char letraFila(int fila) {
@@ -125,7 +118,48 @@ public class Problema5 {
         return letra;
     }
 
-    static void reservar(int[][] sillas, int[] preciosreserva, int[][] precios, int numid, int posid) {
+    static int numfila(char letra) {
+        int fila;
+        switch (letra) {
+            case 'A':
+                fila = 0;
+                break;
+            case 'B':
+                fila = 1;
+                break;
+            case 'C':
+                fila = 2;
+                break;
+            case 'D':
+                fila = 3;
+                break;
+            case 'E':
+                fila = 4;
+                break;
+            case 'F':
+                fila = 5;
+            case 'G':
+                fila = 6;
+                break;
+            case 'H':
+                fila = 7;
+                break;
+            case 'I':
+                fila = 8;
+                break;
+            case 'J':
+                fila = 9;
+                break;
+            case 'K':
+                fila = 10;
+                break;
+            default:
+                throw new AssertionError();
+        }
+        return fila;
+    }
+
+    static int reservar(int[][] sillas, int[][] precios, int numid) {
         Scanner teclado = new Scanner(System.in);
         boolean centinela1 = false;
         boolean centinela = false;
@@ -148,8 +182,9 @@ public class Problema5 {
                         if (centinela == true) {
                             int puestofinal = puestoinicio + 1;
                             puestoinicio = puestofinal - puestosdisponibles;
-                            System.out.println("Hay puestos en la fila " + letraFila(filapuestos) + " de la silla " + puestoinicio + " a la "
-                                    + puestofinal);
+                            System.out.println("Hay puestos en la fila " + letraFila(filapuestos) + " de la silla "
+                                    + (puestoinicio + 1) + " a la " + puestofinal);
+                            puestosdisponibles = 0;
                         }
                         centinela = false;
                     }
@@ -169,8 +204,9 @@ public class Problema5 {
                         if (centinela == true) {
                             int puestofinal = puestoinicio + 1;
                             puestoinicio = puestofinal - puestosdisponibles;
-                            System.out.println("Hay puestos en la fila " + letraFila(filapuestos) + " de la silla " + puestoinicio + " a la "
-                                    + puestofinal);
+                            System.out.println("Hay puestos en la fila " + letraFila(filapuestos) + " de la silla "
+                                    + (puestoinicio + 1) + " a la " + puestofinal);
+                            puestosdisponibles = 0;
                         }
                         centinela = false;
                     }
@@ -181,24 +217,20 @@ public class Problema5 {
                     break;
             }
         }
-        System.out.println("¿Cuantos puestos necesita?");
-        int cantidadpuestos = teclado.nextInt();
-        if (puestosdisponibles < cantidadpuestos) {
-            System.out.println("No hay suficientes puestos");
-        } else {
-            System.out.println("¿En que fila se reservarán los puestos?");
-            int filareserva = teclado.nextInt();
-            System.out.println("¿A partir de que puesto?");
-            int puestoinicial = teclado.nextInt();
-            System.out.println("¿Hasta que puesto?");
-            int puestofinal = teclado.nextInt();
-            int precioreservacion = 0;
-            for (puestoinicial = puestoinicial - 1; puestoinicial < puestofinal; puestoinicial++) {
-                sillas[filareserva][puestoinicial] = numid;
-                precioreservacion += precios[filareserva][puestoinicial];
-            }
-            preciosreserva[posid] = precioreservacion;
+        System.out.println("¿En que fila se reservarán los puestos?");
+        char filareserva = teclado.next().charAt(0);
+        int fila = numfila(filareserva);
+        System.out.println("¿A partir de que puesto?");
+        int puestoinicial = teclado.nextInt();
+        System.out.println("¿Hasta que puesto?");
+        int puestofinal = teclado.nextInt();
+        int precioreservacion = 0;
+        for (puestoinicial = puestoinicial - 1; puestoinicial < puestofinal; puestoinicial++) {
+            sillas[fila][puestoinicial] = numid;
+            precioreservacion += precios[fila][puestoinicial];
         }
+        return precioreservacion;
+
     }
 
     static void recargas(int[] tarcine, int[] id, int numid) {
@@ -265,118 +297,138 @@ public class Problema5 {
         while (centinela == true) {
             System.out.println("¿Reserva, Venta o Ninguna? (r/v/n)");
             char resp = teclado.next().charAt(0);
-            if (resp == 'r') {
-                System.out.println("¿Desea Cancelar alguna reservación? (y / n)");
-                char cancelar = teclado.next().charAt(0);
-                if (cancelar == 'y') {
-                    cancelarReserva(sillas, preciosreserva, id);
-                    System.out.println("Se ha cancelado la reservación");
-                } else {
-                    System.out.println("¿Ya tiene la tarjeta TARCINE?");
-                    char resp1 = teclado.next().charAt(0);
-                    char resp2;
-                    if (resp1 == 'y') {
-                        System.out.println("¿Desea recargarla?");
-                        resp2 = teclado.next().charAt(0);
-                        int numid = 0;
-                        if (resp2 == 'y') {
-                            System.out.println("¿Cual es el número de identificación?");
-                            numid = teclado.nextInt();
-                            recargas(tarcine, id, numid);
-                        } else {
-                            System.out.println("¿Cual es el número de identificación?");
-                            numid = teclado.nextInt();
-                        }
-                        int posid = buscarPosID(id, numid);
-                        reservar(sillas, preciosreserva, precios, numid, posid);
+            //Segmento de codigo para reservar y cancelar reservación
+            switch (resp) {
+                case 'r':
+                    System.out.println("¿Desea Cancelar alguna reservación? (y / n)");
+                    char cancelar = teclado.next().charAt(0);
+                    if (cancelar == 'y') {
+                        cancelarReserva(sillas, preciosreserva, id);
+                        System.out.println("Se ha cancelado la reservación");
                     } else {
-                        System.out.println("¿Desea Comprar una? (y/n)");
-                        resp2 = teclado.next().charAt(0);
-                        if (resp2 == 'y') {
-                            System.out.println("¿Cuál es el numero de identificación?");
-                            int numid = teclado.nextInt();
-                            id = addID(id, numid);
-                            tarcine = addTARCINE(tarcine);
+                        //Segmento para comprar o recargar
+                        System.out.println("¿Ya tiene la tarjeta TARCINE?");
+                        char resp1 = teclado.next().charAt(0);
+                        char resp2;
+                        if (resp1 == 'y') {
+                            System.out.println("¿Desea recargarla? (y/n)");
+                            resp2 = teclado.next().charAt(0);
+                            int numid;
+                            if (resp2 == 'y') {
+                                System.out.println("¿Cual es el número de identificación?");
+                                numid = teclado.nextInt();
+                                recargas(tarcine, id, numid);
+                            } else {
+                                System.out.println("¿Cual es el número de identificación?");
+                                numid = teclado.nextInt();
+                            }
                             int posid = buscarPosID(id, numid);
-                            reservar(sillas, preciosreserva, precios, numid, posid);
+                            int precioreservacion = reservar(sillas, precios, numid);
+                            preciosreserva[posid] = precioreservacion;
                         } else {
-                            System.out.println("No es posible hacer la reserva sin la tarjeta TARCINE");
+                            System.out.println("¿Desea Comprar una? (y/n)");
+                            resp2 = teclado.next().charAt(0);
+                            if (resp2 == 'y') {
+                                System.out.println("¿Cuál es el numero de identificación?");
+                                int numid = teclado.nextInt();
+                                id = addInfo(numid, id);
+                                tarcine = addInfo(70000, tarcine);
+                                int precioreservacion = reservar(sillas, precios, numid);
+                                preciosreserva = addInfo(precioreservacion, preciosreserva);
+                            } else {
+                                System.out.println("No es posible hacer la reserva sin la tarjeta TARCINE");
+                            }
                         }
                     }
-                }
-            } else if (resp == 'v') {
-                int valor = 0;
-                System.out.println("Tienes alguna reserva? (y / n)");
-                char resp3 = teclado.next().charAt(0);
-                if (resp3 == 'y') {
-                    System.out.println("¿Cual es su número de identificación?");
-                    int numid = teclado.nextInt();
-                    buscaReserva(sillas, numid);
-                    int posid = buscarPosID(id, numid);
-                    boolean pago = false;
-                    while (pago == false) {
-                        System.out.println("Medio de pago: 1.Efectivo   2.TARCINE   3.Cancelar Reserva");
-                        int medio = teclado.nextInt();
-                        switch (medio) {
-                            case 1:
-                                System.out.print("El valor a cancelar es: " + preciosreserva[posid] + " Cancelar: ");
-                                valor = teclado.nextInt();
-                                if (valor == preciosreserva[posid]) {
-                                    pago = true;
-                                } else {
-                                }
-                                break;
-                            case 2:
-                                System.out.println("El valor a cancelar es: " + preciosreserva[posid]);
-                                valor = preciosreserva[posid];
-                                if (tarcine[posid] < valor) {
-                                    System.out.println("Su tarjeta no tiene suficiente saldo");
-                                    pago = true;
-                                } else {
-                                    tarcine[posid] = tarcine[posid] - valor;
-                                    pago = true;
-                                }
-                                break;
-                            case 3:
-                                cancelarReserva(sillas, preciosreserva, id);
-                                System.out.println("Cancelación exitosa");
-                                break;
-                            default:
-                                System.out.println("Comando equivocado");
-                                break;
+                    //Segmento de ventas
+                    break;
+                case 'v':
+                    int valor = 0;
+                    System.out.println("Tienes alguna reserva? (y / n)");
+                    char resp3 = teclado.next().charAt(0);
+                    if (resp3 == 'y') {
+                        System.out.println("¿Cual es su número de identificación?");
+                        int numid = teclado.nextInt();
+                        buscaReserva(sillas, numid);
+                        int posid = buscarPosID(id, numid);
+                        boolean pago = false;
+                        while (pago == false) {
+                            // Pago en Efectivo o con la tarjeta TARCINE
+                            System.out.println("Medio de pago: 1.Efectivo   2.TARCINE   3.Cancelar Reserva");
+                            int medio = teclado.nextInt();
+                            switch (medio) {
+                                case 1:
+                                    System.out.print("El valor a cancelar es: " + preciosreserva[posid] + " Cancelar: ");
+                                    valor = teclado.nextInt();
+                                    if (valor == preciosreserva[posid]) {
+                                        pago = true;
+                                    } else {
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("El valor a cancelar es: " + preciosreserva[posid]);
+                                    valor = preciosreserva[posid];
+                                    if (tarcine[posid] < valor) {
+                                        System.out.println("Su tarjeta no tiene suficiente saldo");
+                                        pago = true;
+                                    } else {
+                                        tarcine[posid] = tarcine[posid] - valor;
+                                        pago = true;
+                                    }
+                                    break;
+                                case 3:
+                                    cancelarReserva(sillas, preciosreserva, id);
+                                    System.out.println("Cancelación exitosa");
+                                    break;
+                                default:
+                                    System.out.println("Comando equivocado");
+                                    break;
+                            }
                         }
-                    }
-                } else {
-                    int[] puestosdisponibles = sillasDisponibles(sillas);
-                    System.out.println("¿En que fila se reservarán los puestos?");
-                    int filapuestos = teclado.nextInt();
-                    System.out.println("¿Cuantos puestos necesita?");
-                    int cantidadpuestos = teclado.nextInt();
-                    if (puestosdisponibles[filapuestos] < cantidadpuestos) {
-                        System.out.println("No hay suficientes puestos");
                     } else {
-                        System.out.println("¿A partir de que puesto?");
-                        int puestoinicial = teclado.nextInt();
-                        int precioventa = 0;
-                        System.out.println("¿Hasta que puesto?");
-                        int puestofinal = teclado.nextInt();
-                        for (puestoinicial = puestoinicial - 1; puestoinicial < puestofinal; puestoinicial++) {
-                            sillas[filapuestos][puestoinicial] = 1;
-                            precioventa = precioventa + precios[filapuestos][puestoinicial];
+                        int[] puestosdisponibles = sillasDisponibles(sillas);
+                        System.out.println("¿En que fila se apartarán los puestos?");
+                        char filapuestos = teclado.next().charAt(0);
+                        int fila = numfila(filapuestos);
+                        System.out.println("¿Cuantos puestos necesita?");
+                        int cantidadpuestos = teclado.nextInt();
+                        if (puestosdisponibles[fila] < cantidadpuestos) {
+                            System.out.println("No hay suficientes puestos");
+                        } else {
+                            System.out.println("¿A partir de que puesto?");
+                            int puestoinicial = teclado.nextInt();
+                            int precioventa = 0;
+                            System.out.println("¿Hasta que puesto?");
+                            int puestofinal = teclado.nextInt();
+                            for (puestoinicial = puestoinicial - 1; puestoinicial < puestofinal; puestoinicial++) {
+                                sillas[fila][puestoinicial] = 1;
+                                precioventa = precioventa + precios[fila][puestoinicial];
+                            }
+                            boolean pago = false;
+                            while (pago == false) {
+                                System.out.println("¿Cuanto cancela?");
+                                valor = teclado.nextInt();
+                                if (valor < precioventa) {
+                                    pago = true;
+                                    valor = precioventa;
+                                }else{
+                                    System.out.println("Ese no es el valor de la venta...Ingrese tal valor");
+                                }
+                            }                            
                         }
-                        valor = precioventa;
                     }
-                }
-                totales = addInfo(valor, totales);
-            } else {
-                centinela = false;
+                    totales = addInfo(valor, totales);
+                    break;
+                default:
+                    centinela = false;
+                    break;
             }
         }
         int total = 0;
         for (int pos = 0; pos < totales.length; pos++) {
             total += totales[pos];
         }
-        System.out.println("Las ganacias de hoy fueron: " + totales);
+        System.out.println("Las ganacias de hoy fueron: " + total);
     }
 
     public static void main(String[] args) {
